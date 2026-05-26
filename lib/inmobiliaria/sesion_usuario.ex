ae313418bfefdb9b9ead87b648d7e1e369d32421
Juan Pablo Londoño Gomez <juanp.londonog@uqvirtual.edu.co>
2026-05-26 11:15:30 -0500

    Actualización de código

--- lib/inmobiliaria/sesion_usuario.ex
+++ lib/inmobiliaria/sesion_usuario.ex
@@ -2,7 +2,6 @@ defmodule Inmobiliaria.SesionUsuario do
   use Agent
 
   def start_link(_opts) do
-    # Esto inicia el Agente con el estado inicial y le da nombre al proceso
     Agent.start_link(fn -> %{usuario: nil, rol: nil} end, name: __MODULE__)
   end
 end
